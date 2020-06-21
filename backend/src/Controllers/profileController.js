62e2e34c16afa650379675dbd9cc352ff7d94063
Luis Felipe <luisfelipe.sb@outlook.com>
2020-06-21 02:44:29 -0300

    Criaçao da versão app mobile - Definição das rotas

--- backend/src/Controllers/profileController.js
+++ backend/src/Controllers/profileController.js
@@ -8,7 +8,7 @@ module.exports = {
 
         const incidents = await connection('incidents')
             .where('ong_id', ong_id)
-            .select('title');
+            .select('*');
 
             return response.json(incidents);
     }
